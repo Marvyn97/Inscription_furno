3e6daba41c31c05f2f1d17ba1f786cb3d5926f39
Marvyn PAULIN <mpaulin@groupe-furnotel.com>
2023-06-22 16:22:10 +0200

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='flex flex-col items-center border-b-2 p-4'>
+      <h3 className='inline-flex animate-text-gradient bg-gradient-to-r from-[#b2a8fd] via-[#8678f9] to-[#c7d2fe] bg-[200%_auto] bg-clip-text text-xl text-transparent '>Inscription pour le bien être de votre corp</h3>
+    </div>
+  );
+};
+
+
